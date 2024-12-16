0dff0807d38f7ecbbf69399c5ded2a852d360ec8
adkap2 <adkap2@gmail.com>
2024-12-15 16:57:27 -0800

    First pass at a hub spoke NN for jax

--- models/equinox_models/aa_encoder.py
+++ models/equinox_models/aa_encoder.py
@@ -14,15 +14,13 @@ from typing import List, Tuple, Optional, Union
 
 import numpy as np
 from utils import print_array_type
-
+import random
 
 # Add jax type signature to inputs and outputs
 
 
 class ReLU(eqx.Module):
-    def __call__(self,
-                  x: Float[Array, "batch 8"],
-                    key=None):
+    def __call__(self, x: Float[Array, "batch 8"], key=None):
         output = jax.nn.relu(x)  # Float[Array, "batch 8"]
         return output
 
@@ -44,9 +42,7 @@ class MLP(eqx.Module):
 
     # @beartype
     def __call__(
-        self, 
-        nodes,  # Shape: [batch_size, node_dim=2] -> Float[Array, "2 2"]
-        key
+        self, nodes, key  # Shape: [batch_size, node_dim=2] -> Float[Array, "2 2"]
     ) -> Float[Array, "2 2"]:  # Shape: [batch_size, node_dim=2] -> Float[Array, "2 2"]
 
         key1, key2 = jax.random.split(key)
@@ -83,8 +79,9 @@ class AAEncoder(eqx.Module):
     dropout: float
     mlp: eqx.nn.Sequential
     bos_token: jnp.ndarray
+    max_radius: int
 
-    # @beartype
+    @beartype
     def __init__(
         self,
         historical_steps: int,
@@ -99,6 +96,7 @@ class AAEncoder(eqx.Module):
 
         keys = jax.random.split(key, 12)
 
+        self.max_radius = 50
         # print(f"keys shape: {keys.shape}")
         self.historical_steps = historical_steps
         self.embed_dim = embed_dim
@@ -140,260 +138,107 @@ class AAEncoder(eqx.Module):
         # TODO: Add initialization for the weights
         # self.apply(init_weights)
 
-
-    #TODO def hub_spoke_nn(self,
-    # idx: int,
-    # *,
-    # rot_mats: Optional[Float[Array, "node 2 2"]] = None,
-    #nodes: Float[Array, "node node_dim=2"],
-    # adj_mat: Bool[Array, "node node"],
-
-
-    # node = nodes[idx]
-    # neighbors = nodes[adj_mat[idx]]
-    # R = rots_mats[idx]
-
-    # # 2. Translate to node centric frame
-    # ## 2a center on node
-
-    # ## 2b apply rotation matrices
-
-    # node = node@R
-    # neighbors = jax.vmap(lambda n: n @ R)(neighbors)
-
-    # center_embed = self._center_embed(node)
-
-
-     
-
-
-
-
-    #  node: Float[Array, "node_dim=2"],
-    #  adj_row: Bool[Array, "node"],
-    #  nodes: Float[Array, "node node_dim=2"]) -> Float[Array, "hidden_dim"]
-
-    @beartype
+    # @beartype
     def __call__(
         self,
-        nodes: Float[Array, "batch_size node_dim=2"],  # Shape: [batch_size, node_dim=2]
-        edge_index: Int[Array, "2 num_edges"],  # shape: [2, num_edges]
-        edge_attr: Float[Array, "num_edges 2"],  # Shape: [num_edges, edge_dim]
-        bos_mask: Bool[Array, "batch_size"],  # Shape: [batch_size]
+        positions: Float[
+            Array, "N 50 2"
+        ],  # Full trajectories [Num_nodes, timesteps, xy]
+        bos_mask: Bool[Array, "N 20"],  # Shape: [Numnodes, timesteps]
         t: Optional[int] = None,  # Optional[int]
-        rotate_mat: Optional[Float[Array, "batch_size 2 2"]] = None,  # shape: [batch_size, embed_dim, embed_dim]
-        size=None,
     ):
 
-        
-        #TODO message_passing = jax.vmap(sel.hub_spoke_nn)
-        #Todo latent = message_passing(nodes, adj_mat)
+        node_indices = jnp.arange(positions.shape[0])
+        # outputs = jax.vmap(
+        #     lambda idx: self.hub_spoke_nn(
+        #         idx=idx,
+        #         positions=positions,
+        #         t=t,
+        #         padding_mask=bos_mask,
+        #     )
+        # )(node_indices) # Calls function for each node
+        # Start with regular loop
+        outputs = []
+        for idx in node_indices:
+            outputs.append(self.hub_spoke_nn(idx, positions, t, bos_mask))
+        return outputs
 
-        # CHeange type signature to [List] node features, edge features,
-        # Break the call function into three steps
-        # 1. Center Embedding
-        # 2. Neighbor Embedding
-        # 3. Multi-Head Attention propagate messages
-
-        # Apply message function on all neighbors (just a vmap)
-
-        # After when you do the aggregation, just a sum over all the messages where you take an inner product with the row and the vector messages
-        # Altertively you can use where. If its 1 or 0 and when if it's 0 then you just 0 it.
-        # Keep it in form [node, node, channel]
-        # After applyig sum it will collopse to [node, channel] # Einops sum
-
-        # 4. Feedforward
-        # 5. Residual Connection
-        # 6. Layer Norm
-
-        # TODO Rotation matrix is always a 2x2 matrix
-        # TODO this should be a tensor containing 2x2 matricies
-        # TODO edges themselves are just 2x2 since they are jsut distances
-        # Want to apply rotation matrix to every node.
-        # Vmapping over the matrix multiplication operator
-
-        # print("EQX X", x)
-
-        if rotate_mat is None:
-            # print("x.shape", x.shape)
-            center_embed = self._center_embed(nodes)
-        else:
-            # TODO why do we have to expand dims to match rotation matrix
-            # Look at what the dimensions represent
-
-            # Do the vmap right here instead of rotation matrix:
-            x_rotated = jax.vmap(lambda x, m: x @ m)(
-                nodes, rotate_mat
-            )  # Float[Array, "2 2"]
-
-            # Vmap over the rotation matrix
-            center_embed = jax.vmap(self._center_embed)(
-                x_rotated
-            )  # Float[Array, "2 2"]
-
-        # Apply bos mask
-        # breakpoint()
-        # Apply bos mask using einops
-        # Fix shapes for broadcasting
-        bos_mask = rearrange(bos_mask, "n -> n 1")  # Float[Array, "2 1"]
-
-        center_embed = jnp.where(
-            bos_mask, self.bos_token[t], center_embed
-        )  # Float[Array, "2 2"]
-
-        center_embed = center_embed + self.create_message(
-            jax.vmap(self.norm1)(center_embed), nodes, edge_index, edge_attr, rotate_mat
-        )  # Float[Array, "2 2"]
-
-        center_embed = jax.vmap(self.norm2)(center_embed)  # Float[Array, "2 2"]
-
-        # TODO Talk to Marcell about this approach for handling keys with batch size
-        # Testing
-        batch_size = center_embed.shape[0]  # Int
-        mlp_keys = jax.random.split(
-            jax.random.PRNGKey(0), batch_size
-        )  # PRNGKeyArray[Array, "2 2"]
-
-        center_embed = center_embed + jax.vmap(lambda x, k: self.mlp(x, k))(
-            center_embed, mlp_keys
-        )  # Float[Array, "2 2"]
-
-        return center_embed
-
-    @beartype
-    def create_message(
+    def hub_spoke_nn(
         self,
-        center_embed: Float[Array, "2 2"],  # Shape: [batch_size, embed_dim]
-        nodes: Float[Array, "2 node_dim=2"],  # Shape:  # Shape: [node, node_dim=2] Should be Float[Array, "node node_dim=2"]
-        edge_index: Int[Array, "2 num_edges"],  # Shape: [2, num_edges] # This should be neighbors. For each node you should have a list of neighbors Adjacency matrix Bool[Array, "node node"]
-        edge_attr: Float[Array, "num_edges edge_dim=2"],  # Shape: [num_edges, edge_dim] # Then we can ignore this one  because we have an adjmatrix
-        rotate_mat: Float[Array, "batch_size 2 2"], # Shape: [batch_size, embed_dim, embed_dim]
-    ): # -> Float[Array, "2 2 1"]
-        # TODO replace x with node as that is a better name (List of nodes)
-        # Rotation matrix is a [2,2] tensor
-        # All 2,2 tensors
-        # TODO switch to rel pos 2 neightbor
-        center_embed_i = center_embed[edge_index[1]]  # target nodes, equivalent to center_embed_i in PyTorch Graph
-        # Create a message funiton
-        # First rotate the relative position by the rotation matrix
+        idx: int,
+        positions: Float[Array, "N t=50 2"],
+        t: int,
+        padding_mask: Bool[Array, "node 50"],
+    ) -> Float[Array, "hidden_dim"]:
 
-        x_j = nodes[edge_index[0]]  # Get source node features Float[Array, "2 2"]
+        if t < 1:
+            return jnp.zeros(positions.shape[0])
 
-        # Get center rotate mat
-        center_rotate_mat = rotate_mat[edge_index[1]]  # Float[Array, "2 2 2"]
+        dpositions = positions[:, t, :] - positions[:, t - 1, :]
 
-        # Rotate node features
-        x_rotated = (
-            rearrange(x_j, "n f -> n 1 f") @ center_rotate_mat
-        )  # Float[Array, "2 1 2"]
-        x_rotated = rearrange(x_rotated, "n 1 f -> n f")  # Float[Array, "2 2"]
+        adj_mat = self.create_adj_matrix(
+            positions[:, t, :], padding_mask[:, t]
+        )  # Set all to true bool
 
-        # Rotate edge features
-        edge_rotated = (
-            rearrange(edge_attr, "n f -> n 1 f") @ center_rotate_mat
-        )  # Float[Array, "2 1 2"]
+        # 3. Get hub and spoke data
+        node_xy = positions[idx, t, :]  # hub position
+        node_dxy = dpositions[idx, :]  # hub vel
 
-        edge_rotated = rearrange(edge_rotated, "n 1 f -> n f")  # Float[Array, "2 2"]
+        rot_mat = jnp.eye(2)
+        neighbors_xy = positions[adj_mat[idx]]  # <--- spokes
+        neighbors_dxy = dpositions[adj_mat[idx]]
 
-        # Compute neighbor embedding
-        # nbr_embed = self._nbr_embed([x_rotated, edge_rotated])
-        # Do vmap here
-        # nbr_embed = jax.vmap(self._nbr_embed)([x_rotated, edge_rotated])
-        # x_rotated: [batch_size, node_dim]
+        neighbors_xy = jax.vmap(lambda xy: xy - node_xy)(neighbors_xy)
+        del node_xy  # Don't need node_xy any more. It should be (0,0)
+        node_dxy = node_dxy @ rot_mat
 
-        nbr_embed = jax.vmap(lambda x, e: self._nbr_embed([x, e]))(
-            x_rotated, edge_rotated
-        )  # Float[Array, "2 2"]
+        neighbors_xy = jax.vmap(lambda n: n @ rot_mat)(neighbors_xy)
 
+        center_embed = self._center_embed(node_dxy)
 
-        # print("nbr_embed.shape", nbr_embed.shape)
-        # print("center_embed.shape", center_embed.shape)
-        # print()
-        # breakpoint()
-        # Jax random key is different than torch so can expect slightly different results on the normalization
-        ############################################################################################
-        query = rearrange(
-            jax.vmap(lambda x: self.lin_q(x))(center_embed_i),
-            "n (h d) -> n h d",
-            h=self.num_heads,
-        )  # Float[Array, "2 2 1"]
+        nbr_embed = jax.vmap(lambda a, b: self._nbr_embed([a, b]))(
+            neighbors_xy[:, t, :], neighbors_dxy
+        )
+        print("nbr_embed", nbr_embed.shape)
+        print("nbr_embed", nbr_embed)
+        breakpoint()
 
+    def create_adj_matrix(
+        self,
+        positions: Float[Array, "N 2"],  # positions
+        padding_mask: Bool[Array, "N"],  # mask at current timestep
+    ) -> Bool[Array, "node node"]:
+        """Creates adjacency matrix for nodes within max_radius and not padded."""
+        # 1. Compute pairwise distances between all nodes
+        # Expand dimensions for broadcasting
+        pos_i = positions[:, None, :]  # [N, 1, 2]
+        pos_j = positions[None, :, :]  # [1, N, 2]
 
-        # Jax random key is different than torch so can expect slightly different results on the normalization
+        # Their difference gives [node, node, 2] representing vectors between all pairs
+        diff = pos_i - pos_j  # [N, N, 2]
 
-        # old way
-        # key = rearrange(self.lin_k(nbr_embed), "n (h d) -> n h d", h=self.num_heads)
+        # 2. Get actual distances between nodes
+        distances = jnp.sqrt(jnp.sum(diff**2, axis=-1))  # [node, node]
 
-        key = rearrange(
-            jax.vmap(lambda x: self.lin_k(x))(nbr_embed),
-            "n (h d) -> n h d",
-            h=self.num_heads,
-        )  # Float[Array, "2 2 1"]
+        # 3. Create three mask conditions:
+        # a. Distance threshold
+        dist_mask = distances <= self.max_radius
 
-        # TODO check if this is correct with Marcell
-        # Jax random key is different than torch so can expect slightly different results on the normalization
-        value = rearrange(
-            jax.vmap(lambda x: self.lin_v(x))(nbr_embed),
-            "n (h d) -> n h d",
-            h=self.num_heads,
-        )  # Float[Array, "2 2 1"]
+        # b. Both nodes must be valid (not padded)
+        valid_mask = ~padding_mask[:, None] & ~padding_mask[None, :]
 
-        scale = (self.embed_dim // self.num_heads) ** 0.5  # Float
+        # c. No self-connections
+        ### Set alll top true
+        no_self = ~jnp.eye(positions.shape[0], dtype=bool)
+
+        # 4. Combine all conditions
+        adj_mat = ~(dist_mask & valid_mask & no_self).astype(bool)
+
+        return adj_mat
+
+    def compute_rotation_matrices(self, positions, padding_mask, adj_mat):
+        pass
 
 
-
-
-        alpha = (query * key).sum(axis=-1) / scale  # Float[Array, "2 2"]
-
-        # Do softmax
-        alpha = jax.nn.softmax(alpha)  # Float[Array, "2 2"]
-
-        # TODO Pass a key into this function as an additional random key
-        alpha = self.attn_dropout(
-            alpha, key=jax.random.PRNGKey(0)
-        )  # Float[Array, "2 2"]
-
-        messages = value * rearrange(alpha, "n h -> n h 1")  # Float[Array, "2 2 1"]
-        ############################################################################################
-
-        # aggregate
-        # messages = reduce(
-        #     messages, "n h d -> n d", "sum"
-        # )  # This reduces to [b, d] # Float[Array, "2 1"]
-
-        # This is done under the hood in torch the aggregation
-        out = jax.ops.segment_sum(
-            messages,
-            edge_index[1],  # segment ids (target nodes)
-            num_segments=nodes.shape[0]  # total number of nodes
-        )  # Shape: [num_nodes, num_heads, head_dim]
-
-
-        messages = rearrange(out, 'n h d -> n (h d)')  # Shape: [num_nodes, embed_dim]
-        ## PROPAGATION IS DONE
-
-        # Do equivilant of self.out_proj
-        # Which is linear transformation to aggregated messaged
-
-        messages = self.out_proj(messages)  # Float[Array, "2 2"]
-
-        # Apply dropout
-        # TODO pass a key into this function as an additional random key
-        messages = self.proj_drop(
-            messages, key=jax.random.PRNGKey(0)
-        )  # Float[Array, "2 2"]
-
-        # THen update logic to add to center_embed
-        gate = jax.nn.sigmoid(
-            self.lin_ih(messages) + self.lin_hh(center_embed)
-        )  # Float[Array, "2 2"]
-
-        messages = messages + gate * (
-            self.lin_self(center_embed) - messages
-        )  # Float[Array, "2 2"]
-
-        print("EQX outputs", messages)
-        return messages
-
-        # Everything goes into message function
-        # center_rotate_mat = rotate_mat[edge_index[1]]
+# TODO write a function that creates an adjaceny matrix for time t it will then determine if padding mask is true or false. A node wont be connected if it is padded. Filter out things that are padding and things that are too far. This will tell us what are the hubs and spokes.
+# Ignore x and just use the positions.
