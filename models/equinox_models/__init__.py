90845f7dad214a7d646293bf62b257d4f8623794
adkap2 <adkap2@gmail.com>
2025-01-19 16:37:24 -0800

    Refactoring for more reusability

--- models/equinox_models/__init__.py
+++ models/equinox_models/__init__.py
@@ -1,7 +1,16 @@
 from models.equinox_models.embedding import SingleInputEmbedding
 from models.equinox_models.aa_encoder import AAEncoder
+from models.equinox_models.mlp import MLP, ReLU
+from models.equinox_models.temporal_encoder import TemporalEncoder
+from models.equinox_models.local_encoder import LocalEncoder
+from models.equinox_models.al_encoder import ALEncoder
 
 __all__ = [
     'SingleInputEmbedding',
-    'AAEncoder'
+    'AAEncoder',
+    'MLP',
+    'ReLU',
+    'TemporalEncoder',
+    'LocalEncoder',
+    'ALEncoder'
 ] 
---
+++ models/equinox_models/mlp.py
@@ -0,0 +1,59 @@
+import equinox as eqx
+import jax
+import jax.numpy as jnp
+from typing import Optional, Tuple, List
+from jaxtyping import Array, Float, PRNGKeyArray, Int, Bool, Union, Scalar
+from models.equinox_models.embedding import SingleInputEmbedding, MultipleInputEmbedding
+
+from einops import rearrange, reduce
+
+# Import jnp
+
+# Import beartype
+from beartype import beartype
+from typing import List, Tuple, Optional
+
+from utils import print_array_type
+
+# Add jax type signature to inputs and outputs
+
+
+
+
+class ReLU(eqx.Module):
+    def __call__(self, x: Float[Array, "batch=2 8"], key=None):
+        output = jax.nn.relu(x)  # Float[Array, "batch 8"]
+        return output
+
+
+
+class MLP(eqx.Module):
+    linear1: eqx.nn.Linear
+    linear2: eqx.nn.Linear
+    dropout1: eqx.nn.Dropout
+    dropout2: eqx.nn.Dropout
+    relu: ReLU
+
+    # @beartype
+    def __init__(self, embed_dim: int, dropout_rate: float, keys: PRNGKeyArray):
+        self.linear1 = eqx.nn.Linear(embed_dim, embed_dim * 4, key=keys[0])
+        self.linear2 = eqx.nn.Linear(embed_dim * 4, embed_dim, key=keys[1])
+        self.dropout1 = eqx.nn.Dropout(dropout_rate)
+        self.dropout2 = eqx.nn.Dropout(dropout_rate)
+        self.relu = ReLU()
+
+    # @beartype
+    def __call__(
+        self, nodes, key  # Shape: [batch_size, node_dim=2] -> Float[Array, "2 2"]
+    ) -> Float[Array, "batch=2 node_dim=2"]:  # Shape: [batch_size, node_dim=2] -> Float[Array, "2 2"]
+
+        key1, key2 = jax.random.split(key)
+        nodes = self.linear1(nodes)  # Float[Array, "batch=2 num_heads=8"]
+
+        nodes = self.relu(nodes)  # Float[Array, "batch 8"]
+
+        nodes = self.dropout1(nodes, key=key1)  # Float[Array, "batch 8"]
+
+        nodes = self.linear2(nodes)  # Float[Array, "batch 2"]
+        nodes = self.dropout2(nodes, key=key2)  # Float[Array, "batch node_dim=2"]
+        return nodes
